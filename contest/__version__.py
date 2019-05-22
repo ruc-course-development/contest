2d566a1b47854fbf8d319742c8fa78f9ac822748
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-05-22 16:37:17 -0400

    Add initial setup.py script

---
+++ contest/__version__.py
@@ -0,0 +1,6 @@
+__title__ = 'contest'
+__description__ = 'A console application tester.'
+__url__ = 'https://github.com/Lnk2past/contest'
+__version__ = '2019.1.0'
+__author__ = 'Lnk2past'
+__license__ = 'MIT'
