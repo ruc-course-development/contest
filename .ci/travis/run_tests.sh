cf4e4090b05790095a767e0ffb3115cec4cb6e9a
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-05-22 16:29:47 -0400

    Fix travis test script

--- .ci/travis/run_tests.sh
+++ .ci/travis/run_tests.sh
@@ -1,5 +1,5 @@
 set -x
-python contest.py examples/many_cases/contest_recipe.yaml
-python contest.py examples/native_console_app/contest_recipe.yaml
-python contest.py examples/output_file/contest_recipe.yaml
-python contest.py examples/python_console_app/contest_recipe.yaml
+python contest/runner.py examples/many_cases/contest_recipe.yaml
+python contest/runner.py examples/native_console_app/contest_recipe.yaml
+python contest/runner.py examples/output_file/contest_recipe.yaml
+python contest/runner.py examples/python_console_app/contest_recipe.yaml
