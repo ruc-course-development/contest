cfb1547fdb830496288f5d763d3f7e8efa586480
Lnk2past <Lnk2past@gmail.com>
2019-09-02 21:35:13 -0400

    Update version to 2019.9.0

--- contest/__version__.py
+++ contest/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'contest'
 __description__ = 'A console application tester.'
 __url__ = 'https://github.com/Lnk2past/contest'
-__version__ = '2019.1.1'
+__version__ = '2019.9.0'
 __author__ = 'Lnk2past'
 __license__ = 'MIT'
