23e996e0ba100e087fe75aee9ca679c07309ab29
Lnk2past <Lnk2past@gmail.com>
2021-09-22 00:22:49 -0400

    HOTFIX: version...

--- contest/__version__.py
+++ contest/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'contest'
 __description__ = 'A console application tester.'
 __url__ = 'https://github.com/Lnk2past/contest'
-__version__ = '2021.9.5'
+__version__ = '2021.9.7'
 __author__ = 'Lnk2past'
 __license__ = 'MIT'
